d85fe0161a3cda7635b600a208ba2a9c666e4cd7
TLER AL-BISHI <111729973+tlersa@users.noreply.github.com>
2023-03-11 21:01:56 +0300

    Add files via upload
    Regular Calculator Project, Accepts Addition And Subtraction And Multiplication And Division Only.

---
+++ Beginner Level/Calculactor.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int num1,num2;
+
+	printf("Enter Two Numbers : \n");
+	scanf("%d%d",&num1,&num2);
+
+	printf("[+] = %d \n",num1+num2);
+	printf("[-] = %d \n",num1-num2);
+	printf("[/] = %d \n",num1/num2);
+	printf("[*] = %d \n",num1*num2); }
